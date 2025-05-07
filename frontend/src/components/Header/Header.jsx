0906ce1afef7df714919b408460adf2b8f0b0ab4
Sivaharini Elan <shahulyaalh@gmail.com>
2025-05-07 15:52:16 +0530

    update

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -45,7 +45,7 @@ const Header = () => {
                 STAIR ECOSYSTEM
               </span>
 
-              <span className="text-gray-700 font-medium text-xs mt-1">
+              <span className="ml-5text-gray-700 font-medium text-xs mt-1">
                 CIN: U80200TN2025PTC179412
               </span>
             </div>
